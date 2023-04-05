645ccd5680a5618aa78f004aea15765532c55d2c
Niklas <zhoie@icloud.com>
2023-04-05 17:10:33 +0800

    add website url json

--- src/components/WebsiteCard.tsx
+++ src/components/WebsiteCard.tsx
@@ -1,3 +1,42 @@
-export default function Websistecard() {
-    return null;
+import Link from 'next/link'
+import Image from 'next/image'
+
+interface Websites {
+  name: string;
+  url: string;
+  description: string;
+  icon?: string;
+}
+
+
+
+export default function Websistecard({ name, url, description, icon }: Websites) {
+
+  function handleClick() {
+    window.open(url, '_blank')
+  }
+
+  return (
+    <>
+      <div onClick={handleClick} className=" flex lex-row bg-westar-200 w-[80%] h-12 rounded-2xl my-2 mx-10 cursor-pointer hover:scale-110 duration-300">
+        {/* icon */}
+        <div className="rounded-2xl w-12 relative">
+        
+        <img src={url+'/favicon.ico'} alt="icon" className=""/>
+
+        </div>
+        
+        {/* name and descrition */}
+        <div className="flex mt-2 my20">
+          
+          <h1 className='text-2xl '>{name}</h1>
+          {/* <p className='text-2xl'>{description}</p> */}
+          
+          
+        </div>
+
+        
+      </div>
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
+import WebsiteCard from '@/components/WebsiteCard';
+import { describe } from 'node:test';
+
+import { GetStaticProps } from 'next'
 // import styles from '@/styles/Home.module.css'
+
+interface Websites {
+  name: string;
+  url: string;
+  description: string;
+  icon: string;
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const websites: Websites[] = require('./api/websites.json')
+
   return (
     <>
       <Head>
@@ -15,11 +30,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="bg-pampas-50 h-screen bg-gradient-to-b">
 
-      <div>
-      <h1 className='text-green-800'>A Hub Link</h1>
+        <div className='flex flex-col' >
+          <h1 className='flex text-4xl text-black mt-10 mx-auto'>A Hub Link</h1>
+
+          <div className='grid grid-cols-3 gap-4 mt-10'>
+
+            {websites.map((website) => (
+              <div className="flex">
+                <WebsiteCard name={website.name} url={website.url} description={website.description} icon={website.icon} />
+              </div>
+            ))}
+
+          </div>
+        </div>
       </div>
-      
+
     </>
   )
+}
+
+
+//fetch icon and description from api
+
+async function getIcon(url: string) {
+  const res = await fetch(url)
+  const data = await res.json()
+  return data
 }
