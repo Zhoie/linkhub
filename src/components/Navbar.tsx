296b35ef60586997b38deffc1fa976f6f80f760a
Niklas <zhoie@icloud.com>
2023-04-07 00:54:36 +0800

    to combine mutiple JSON files in to one.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <div className="flex flex-row justify-between items-center w-full h-16 bg-white border-b border-gray-200">
+            <nav></nav>
+
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,15 +15,20 @@ interface Websites {
   icon: string;
 }
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
   // {add websites to json file}
-  const aiWebsites: Websites[] = require('./api/websites/aiWebsites.json')
-  const designWebsites: Websites[] = require('./api/websites/designWebsites.json')
-  const Websites = [aiWebsites, designWebsites]
-  const WebsiteTitle = ["Ai", "Design"]
+  // const aiWebsites: Websites[] = require('./api/websites/aiWebsites.json')
+  // const designWebsites: Websites[] = require('./api/websites/designWebsites.json')
+  // const docWebsites: Websites[] = require('./api/websites/docWebsites.json')
+  // const Websites = [aiWebsites, designWebsites, docWebsites]
+  // const WebsiteTitle = ["Ai", "Design", "Docs"]
+
+  const WebsitesArray: [Websites[]] = require('./api/websites.json')
+
   return (
     <>
       <Head>
@@ -34,11 +39,30 @@ export default function Home() {
       </Head>
 
       <div className="bg-pampas-50 h-screen bg-gradient-to-b">
-
         <div className='flex flex-col' >
           <h1 className='flex justify-center w-full text-4xl font-bold text-westar-950 my-5 '>Link Hub</h1>
 
-          {Websites.map((websiteArray, index) => (
+
+          {Object.values(WebsitesArray).map((websites, index) => (
+            <div key={index}>
+
+              <h1 className='flex justify-center w-full bg-westar-950 py-2 text-pampas-50 text-3xl font-semibold'>{Object.keys(WebsitesArray)[index]}</h1>
+              <div className='grid grid-flow-col-dense mb-20 mt-4 last:mr-2'>
+                {websites.map((website, index) => (
+
+                  <div key={index} className="flex">
+
+                    <div key={website.url} className="flex">
+                      <WebsiteCard key={website.url} name={website.name} url={website.url} description={website.description} icon={website.icon} />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+
+
+          {/* {Websites.map((websiteArray, index) => (
             <div key={index}>
               <h1 className='flex justify-center w-full bg-westar-950 py-2 text-pampas-50 text-3xl font-semibold'>{WebsiteTitle[index]}</h1>
               <div className='grid grid-cols-3 mb-20 mt-4'>
@@ -49,25 +73,9 @@ export default function Home() {
                 ))}
               </div>
             </div>
-
-          ))}
-
-          {/* website for Ai tools */}
-          {/* <h1 className='flex w-full bg-westar-950 py-2 text-pampas-50 text-3xl justify-center'>Ai</h1>
-          <div className='grid grid-cols-3 gap-4 mt-10'>
-            {aiWebsites.map((website) => (
-              <div key={website.url} className="flex">
-                <WebsiteCard key={website.url} name={website.name} url={website.url} description={website.description} icon={website.icon} />
-              </div>
-            ))}
-
-
-           
-
-          </div> */}
+          ))}  */}
         </div>
       </div>
-
     </>
   )
 }
@@ -75,8 +83,8 @@ export default function Home() {
 
 //fetch icon and description from api
 
-async function getIcon(url: string) {
-  const res = await fetch(url)
-  const data = await res.json()
-  return data
-}
+// async function getIcon(url: string) {
+//   const res = await fetch(url)
+//   const data = await res.json()
+//   return data
+// }
