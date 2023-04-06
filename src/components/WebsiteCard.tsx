55ec7be24ded811a77e01a64a4c2ac4613b670a7
Niklas <zhoie@icloud.com>
2023-04-06 20:42:14 +0800

    fix: aiWebsite.

--- src/components/WebsiteCard.tsx
+++ src/components/WebsiteCard.tsx
@@ -18,23 +18,10 @@ export default function Websistecard({ name, url, description, icon }: Websites)
 
   return (
     <>
-      <div onClick={handleClick} className=" flex lex-row bg-westar-200 w-[80%] h-12 rounded-2xl my-2 mx-10 cursor-pointer hover:scale-110 duration-300">
-        {/* icon */}
-        <div className="rounded-2xl w-12 relative">
-        
-        <img src={url+'/favicon.ico'} alt="icon" className=""/>
+      <div onClick={handleClick} className=" flex relative w-[80%] h-12 border-westar-950 border-4 my-2 mx-10 cursor-pointer hover:bg-westar-950 duration-300 ">
 
-        </div>
-        
-        {/* name and descrition */}
-        <div className="flex mt-2 my20">
-          
-          <h1 className='text-2xl '>{name}</h1>
-          {/* <p className='text-2xl'>{description}</p> */}
-          
-          
-        </div>
-
+          <h1 className='flex w-full h-full text-2xl text-westar-950 hover:text-pampas-50 static justify-center pt-1'>{name}</h1>
+   
         
       </div>
     </>
