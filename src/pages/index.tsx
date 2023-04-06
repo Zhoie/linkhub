27e8b233776d5d2fb90333ea1d7effbe9a07f672
Niklas <zhoie@icloud.com>
2023-04-07 01:08:09 +0800

    fix: gap for mobile.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,11 +34,11 @@ export default function Home() {
       <div className="bg-pampas-50 h-screen bg-gradient-to-b">
         <div className='flex flex-col' >
           <h1 className='flex justify-center w-full text-4xl font-bold text-westar-950 my-5 '>Link Hub</h1>
-          {/* WEbsiteCard */}
+          {/* WebsiteCard */}
           {Object.values(WebsitesArray).map((websites, index) => (
             <div key={index}>
               <h1 className='flex justify-center w-full bg-westar-950 py-2 text-pampas-50 text-3xl font-semibold'>{Object.keys(WebsitesArray)[index]}</h1>
-              <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mb-20 mt-4 mr-4'>
+              <div className='grid grid-cols-3 gap-1 md:grid-cols-4lg:grid-cols-5 mb-20 mt-4 mr-4'>
                 {websites.map((website, index) => (
                   <div key={index} className="flex">
                     <WebsiteCard key={website.url} name={website.name} url={website.url} description={website.description} icon={website.icon} />
