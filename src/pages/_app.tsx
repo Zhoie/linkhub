7f72298e0caff23826c80386801e2d0534961d24
Niklas <zhoie@icloud.com>
2023-04-19 22:46:50 +0800

    Add; firebase websitee

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Layout from '@/components/global/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  )
 }
